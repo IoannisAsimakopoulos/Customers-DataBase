e1f67372b4a35c112f8d28a481679d927efddb3d
IoannisAsimakopoulos <john@192.168.181.1>
2021-03-15 20:05:30 +0200

    new commit

---
+++ Alis Customers/src/alis/customers/AlisInsCustomers.java
@@ -0,0 +1,11 @@
+
+package alis.customers;
+
+public class AlisInsCustomers {
+
+    
+    public static void main(String[] args) {
+       new catalogMain().setVisible(true); // enable the form) 
+    }
+    
+}
